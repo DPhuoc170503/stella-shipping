02eefb198d9bb6bf380bb0babe40fd050d5a6f2d
Lê Duy Phước <phuoc_2151220055@dau.edu.vn>
2026-08-14 10:16:38 +0700

    Update db.js with SSL and Port

--- backend/db.js
+++ backend/db.js
@@ -4,9 +4,13 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 12924,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'vietlogis_demo',
+  ssl: {
+    rejectUnauthorized: false // Bắt buộc có để kết nối Aiven SSL
+  },
   charset: 'utf8mb4',
   timezone: '+07:00',
   waitForConnections: true,
